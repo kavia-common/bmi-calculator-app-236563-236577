dce4fa4af439bae6bb5610a7754afa08eb8ed652
babu <babu.k@kavia.ai>
2026-02-25 21:17:26 +0000

    Checkpoint 7 - cg52eb4769

--- bmi_calculator_frontend/src/App.js
+++ bmi_calculator_frontend/src/App.js
@@ -38,6 +38,34 @@ function App() {
             src: "/assets/slider-4.png",
             alt: "BMI calculator background 4",
           },
+          {
+            src: "/assets/slider-5.png",
+            alt: "BMI calculator background 5",
+          },
+          {
+            src: "/assets/slider-6.png",
+            alt: "BMI calculator background 6",
+          },
+          {
+            src: "/assets/slider-7.png",
+            alt: "BMI calculator background 7",
+          },
+          {
+            src: "/assets/slider-8.png",
+            alt: "BMI calculator background 8",
+          },
+          {
+            src: "/assets/slider-9.png",
+            alt: "BMI calculator background 9",
+          },
+          {
+            src: "/assets/slider-10.png",
+            alt: "BMI calculator background 10",
+          },
+          {
+            src: "/assets/slider-11.png",
+            alt: "BMI calculator background 11",
+          },
         ]}
         intervalMs={8000}
         showControls={false}
